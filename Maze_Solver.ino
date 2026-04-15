4f84f2cb7ed1207b46958ac9ca1a55b3c3f3a70c
Vishwajit <vishwajitsake@gmail.com>
2026-04-15 23:27:18 +0530

    Speed of the motor is now controllable through the code.
    Variables for the motorspeed is added in the code.

--- Maze_Solver.ino
+++ Maze_Solver.ino
@@ -1,68 +1,71 @@
 #include <Ultrasonic.h>
 
-const int leftSensorTrig = 8;
-const int leftSensorEcho = 9;
-const int frontSensorTrig = 10;
-const int frontSensorEcho = 11;
-const int rightSensorTrig = 12;
-const int rightSensorEcho = 13;
+const int leftSensorTrig    = 8;
+const int leftSensorEcho    = 9;
+const int frontSensorTrig   = 10;
+const int frontSensorEcho   = 11;
+const int rightSensorTrig   = 12;
+const int rightSensorEcho   = 13;
 
-const int motorLeftForward = 2;
-const int motorLeftBackward = 3;
-const int motorRightForward = 4;
-const int motorRightBackward = 5;
+const int motorLeftForward  = 3;
+const int motorLeftBackward = 5;
+const int motorRightForward = 6;
+const int motorRightBackward = 9;
+
+int motorSpeed = 150;
+int turnspeed=120;
 
 Ultrasonic leftSensor(leftSensorTrig, leftSensorEcho);
 Ultrasonic frontSensor(frontSensorTrig, frontSensorEcho);
 Ultrasonic rightSensor(rightSensorTrig, rightSensorEcho);
 
 void setup() {
-  pinMode(motorLeftForward, OUTPUT);
-  pinMode(motorLeftBackward, OUTPUT);
-  pinMode(motorRightForward, OUTPUT);
+  pinMode(motorLeftForward,   OUTPUT);
+  pinMode(motorLeftBackward,  OUTPUT);
+  pinMode(motorRightForward,  OUTPUT);
   pinMode(motorRightBackward, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-int distanceLeft  = leftSensor.read(CM);
-int distanceFront = frontSensor.read(CM);
-int distanceRight = rightSensor.read(CM);
-  if (distanceFront < 15) {
+  int distanceLeft  = leftSensor.read(CM);
+  int distanceFront = frontSensor.read(CM);
+  int distanceRight = rightSensor.read(CM);
 
+  Serial.print("L: "); Serial.print(distanceLeft);
+  Serial.print(" F: "); Serial.print(distanceFront);
+  Serial.print(" R: "); Serial.println(distanceRight);
+
+  if (distanceFront < 15) {
     turnRight();
   } else if (distanceLeft < 10) {
-
     moveForward();
   } else if (distanceRight < 10) {
     moveForward();
   } else {
-
     turnLeft();
   }
 }
 
 void moveForward() {
-  digitalWrite(motorLeftForward, HIGH);
-  digitalWrite(motorRightForward, HIGH);
-  digitalWrite(motorLeftBackward, LOW);
-  digitalWrite(motorRightBackward, LOW);
+  analogWrite(motorLeftForward,   motorSpeed);
+  analogWrite(motorRightForward,  motorSpeed);
+  analogWrite(motorLeftBackward,  0);
+  analogWrite(motorRightBackward, 0);
 }
 
 void turnLeft() {
-  digitalWrite(motorLeftForward, LOW);
-  digitalWrite(motorRightForward, HIGH);
-  digitalWrite(motorLeftBackward, LOW);
-  digitalWrite(motorRightBackward, LOW);
+  analogWrite(motorLeftForward,   0);
+  analogWrite(motorRightForward,  turnspeed);
+  analogWrite(motorLeftBackward,  0);
+  analogWrite(motorRightBackward, 0);
   delay(500);
 }
 
 void turnRight() {
-  digitalWrite(motorLeftForward, HIGH);
-  digitalWrite(motorRightForward, LOW);
-  digitalWrite(motorLeftBackward, LOW);
-  digitalWrite(motorRightBackward, LOW);
+  analogWrite(motorLeftForward,   turnspeed);
+  analogWrite(motorRightForward,  0);
+  analogWrite(motorLeftBackward,  0);
+  analogWrite(motorRightBackward, 0);
   delay(500);
 }
-
-
